0bd5d56552385a32d0815a1b4c8cefee6f896d1e
Muhammad Raza <103913857+mohammadraza123@users.noreply.github.com>
2023-03-20 14:23:34 +0500

    Add files via upload

---
+++ 01  Expanding Cards/app.js
@@ -0,0 +1,17 @@
+const panel = document.querySelectorAll(".panel");
+panel.forEach(panel => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+
+
+function removeActiveClasses() {
+    panel.forEach(panel => {
+
+        panel.classList.remove('active')
+
+    })
+}
